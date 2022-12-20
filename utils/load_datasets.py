9eb98b589b4a53de5cd7b46865a47d7ff1a6ba70
MachacuayJ <110218977+MachacuayJ@users.noreply.github.com>
2022-12-20 16:34:23 -0500

    Update load_datasets.py

--- utils/load_datasets.py
+++ utils/load_datasets.py
@@ -7,7 +7,8 @@ def load_mHealth(subjects=False):
         for i in range(1, 11):
             tmp = pd.read_csv("datasets/mHealth/mHealth_subject{}.csv".format(i),
                               sep="\t", header=None)
-            tmp = tmp[[14,15,16,17,18,19,20,21,22,23]]
+            #tmp = tmp[[14,15,16,17,18,19,20,21,22,23]]  # old
+            tmp = tmp[[0, 1, 2, 17, 18, 19, 20, 21, 22, 23]]  # new
             tmp = tmp[tmp[23] != 0]
             df.append(tmp)
     else:
@@ -15,6 +16,8 @@ def load_mHealth(subjects=False):
         for i in range(2, 11):
             df = pd.concat([df, pd.read_csv("datasets/mHealth/mHealth_subject{}.csv".format(i),
                                             sep="\t", header=None)])
-        df = df[[14,15,16,17,18,19,20,21,22,23]]
+        #df = df[[14,15,16,17,18,19,20,21,22,23]]  # old
+        df = df[[0, 1, 2, 17, 18, 19, 20, 21, 22, 23]]  # new
         df = df[df[23] != 0]
     return df
+
